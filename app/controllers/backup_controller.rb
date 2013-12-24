8fb24782a826c5b6439f975403303415a01435e8
grentis <grentis@gmail.com>
2013-12-24 15:50:28 +0100

    new line export data

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -15,7 +15,7 @@ class BackupController < ApplicationController
 
   def export
     @clients = Client.find(:all, order: :name)
-    content = render_to_string 'backup/export', layout: false
-    send_data content,:type => 'text',:disposition => "attachment; filename=dati_"+ l(Time.now, format: '%d.%m.%Y') +".txt"
+    content = (render_to_string 'backup/export', layout: false).gsub(/\n/, "\r\n")
+    send_data content,:type => 'text/plain; charset=utf-8',:disposition => "attachment; filename=dati_"+ l(Time.now, format: '%d.%m.%Y') +".txt"
   end
 end
